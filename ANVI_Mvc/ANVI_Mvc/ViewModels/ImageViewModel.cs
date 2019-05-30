748f0b32ee0c85a20eb1a0b5523b91b6271c21d6
mireille03021 <33798498+mireille03021@users.noreply.github.com>
2019-05-31 01:32:26 +0800

    修正ProductDetail產品圖片問題

---
+++ ANVI_Mvc/ANVI_Mvc/ViewModels/ImageViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ANVI_Mvc.ViewModels
+{
+    public class ImageViewModel
+    {
+            public int ProductID { get; set; }
+            public int ImgID { get; set; }
+            public string ImgName { get; set; }
+            public string PDID { get; set; }
+    }
+}
