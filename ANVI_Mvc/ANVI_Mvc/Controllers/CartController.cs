d6fb24736c68489aee4377da1241feb906d22ccb
Chan-Feng Tsao <mireille03021@gmail.com>
2019-05-30 19:13:49 +0800

    修正

--- ANVI_Mvc/ANVI_Mvc/Controllers/CartController.cs
+++ ANVI_Mvc/ANVI_Mvc/Controllers/CartController.cs
@@ -67,7 +67,6 @@ namespace ANVI_Mvc.Controllers
             currentCart.AddQuantity(pdid);
             return RedirectToAction("ShoppingCart", "Cart");
         }
-        //123123123123123213123
         [HttpPost]
         public ActionResult ReduceQuantity(string pdid)  //指定物品用
         {
