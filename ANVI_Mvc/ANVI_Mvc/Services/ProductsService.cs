00aa348d909ea6d4f8144dd0d0da0796572b9c7e
mireille03021 <33798498+mireille03021@users.noreply.github.com>
2019-06-03 04:04:23 +0800

    修正GetCartItem和_CartItemPartial

---
+++ ANVI_Mvc/ANVI_Mvc/Services/ProductsService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ANVI_Mvc.Models;
+using ANVI_Mvc.ViewModels;
+
+namespace ANVI_Mvc.Services
+{
+    public class ProductsService
+    {
+        private AnviModel db;
+        public ProductsService(AnviModel db)
+        {
+            this.db = db;
+        }
+        public List<ProductPageViewModel> getPageOfProducts()
+        {
+            var list = from cat in db.Categories
+                join p in db.Products on cat.CategoryID equals p.CategoryID
+                join pd in db.ProductDetails on p.ProductID equals pd.ProductID
+                select new ProductPageViewModel
+                {
+                    ProductID = p.ProductID,
+                    PDID = pd.PDID,
+                    ColorID = pd.ColorID,
+                    CategoryName = cat.CategoryName
+                };
+            return list.ToList();
+        }
+    }
+}
