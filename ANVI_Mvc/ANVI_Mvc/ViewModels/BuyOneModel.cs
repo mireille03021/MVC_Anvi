800f7442639183b7e45d3f2f207e41f3d08ac754
mireille03021 <33798498+mireille03021@users.noreply.github.com>
2019-06-04 23:30:11 +0800

    更正OrderPartial

---
+++ ANVI_Mvc/ANVI_Mvc/ViewModels/BuyOneModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ANVI_Mvc.ViewModels
+{
+    public class BuyOneViewModel
+    {
+        public CartItemViewModel CartItem { get; set; }
+        public string Image { get; set; }
+    }
+}
