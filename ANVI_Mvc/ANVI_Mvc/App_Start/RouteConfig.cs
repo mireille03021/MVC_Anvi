61eb3b2baa11662d65fcfe63d7de7a628a3d4315
Walter <walterzeng1224@outlook.com>
2019-06-05 00:17:44 +0800

    建立商品分類ROUTE

--- ANVI_Mvc/ANVI_Mvc/App_Start/RouteConfig.cs
+++ ANVI_Mvc/ANVI_Mvc/App_Start/RouteConfig.cs
@@ -33,12 +33,50 @@ namespace ANVI_Mvc
                 url: "Home/{action}/{page}",
                 defaults: new { controller = "Home", action = "ProductsPage", page = 1 }
             );
+
+            //預設的產品分類位置
+            routes.MapRoute(
+                name: "CategoryProducts",
+                url: "Category/{action}/{cat}",
+                defaults: new { controller = "Home", action = "ProductsPage"}
+            );
+            //產品分類:手鍊
+            routes.MapRoute(
+                name: "BraceletsProducts",
+                url: "Category/{action}/{cat}",
+                defaults: new { controller = "Catrgory", action = "BraceletsProductsPage", page = 1 }
+            );
+            routes.MapRoute(
+                name: "GetBraceletsProducts",
+                url: "Category/{action}/{cat}",
+                defaults: new { controller = "Catrgory", action = "GetBraceletsProductsPage", cat = "Bracelets" }
+            );
+            ////產品分類:耳環
+            //routes.MapRoute(
+            //    name: "BraceletsProducts",
+            //    url: "Products/Catrgory/{page}",
+            //    defaults: new { controller = "Catrgory", action = "EarRingsProductsPage", page = "EarRings" }
+            //);
+            ////產品分類:項鍊
+            //routes.MapRoute(
+            //    name: "NecklacesProducts",
+            //    url: "Products/Catrgory/{page}",
+            //    defaults: new { controller = "Catrgory", action = "NecklacesProductsPage", page = "Necklaces" }
+            //);
+            ////產品分類:戒指
+            //routes.MapRoute(
+            //    name: "RingsProducts",
+            //    url: "Products/Catrgory/{page}",
+            //    defaults: new { controller = "Catrgory", action = "RingsProductsPage", page = "Rings" }
+            //);
+
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
-            
+          
         }
     }
 }
