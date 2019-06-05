16152afd30a271a6d4a3e42bd84132552838e5dd
Walter <walterzeng1224@outlook.com>
2019-06-05 13:59:59 +0800

    修改分類頁 route

--- ANVI_Mvc/ANVI_Mvc/Controllers/CategoryController.cs
+++ ANVI_Mvc/ANVI_Mvc/Controllers/CategoryController.cs
@@ -20,7 +20,7 @@ namespace ANVI_Mvc.Controllers
         }
         // GET: Catrgory
         [AllowAnonymous]
-        public ActionResult Bracelets(int cat) //手鍊商品頁面
+        public ActionResult Bracelets(int page) //手鍊商品頁面
         {
 
             //抓產品的分類
@@ -32,43 +32,15 @@ namespace ANVI_Mvc.Controllers
 
             //給@Html.Action使用，取得HomeController的GetProducts方法
             ViewBag.Title = "BRACELETS";
-            ViewBag.ActionName = "GetBraceletsProducts";
-            ViewBag.Controller = "Category";
-            ViewBag.Page = cat;
+            ViewBag.ActionName = "GetProducts";
+            ViewBag.Controller = "Home";
+            ViewBag.Page = page;
             return View();
         }
-        [AllowAnonymous]
-        public ActionResult GetBraceletsProducts(int cat, List<Product> AllProducts)
-        {
-            //每八個一頁
-            var ProductNumber = 8;
-            //取得目前頁所需要顯示的物品
-            var pList = AllProducts.Skip((cat - 1) * ProductNumber).Take(cat * ProductNumber).ToList();
-
-            //建立全部商品
-            ProductsService service = new ProductsService(db);
-            var AllProductDetails = service.getPageOfProducts();
-            var Model = new List<ProductPageViewModel>();
-
-            for (var i = 0; i < pList.Count; i++)
-            {
-                //避免超出範圍，雖然不一定會用到
-                if (i % 8 == 0 && i != 0) break;
-
-                //取得此產品ID所有的PDID，並加進List<ProductPageViewModel> Model中
-                var NowPid = pList[i].ProductID;
-                var FilterList = AllProductDetails.Where(x => x.ProductID == NowPid);
-                foreach (var item in FilterList)
-                {
-                    Model.Add(item);
-                }
-            }
-            ViewData.Model = Model;
-            return PartialView("_ProductsPartial");
-        }
+      
 
         [AllowAnonymous]
-        public ActionResult EarRings(int cat) //耳環商品頁面
+        public ActionResult EarRings(int page) //耳環商品頁面
         {
 
             //抓產品的分類
@@ -80,43 +52,14 @@ namespace ANVI_Mvc.Controllers
 
             //給@Html.Action使用，取得HomeController的GetProducts方法
             ViewBag.Title = "EARPINGS";
-            ViewBag.ActionName = "GetEarRingsProducts";
-            ViewBag.Controller = "Category";
-            ViewBag.Page = cat;
+            ViewBag.ActionName = "GetProducts";
+            ViewBag.Controller = "Home";
+            ViewBag.Page = page;
             return View();
         }
+        
         [AllowAnonymous]
-        public ActionResult GetEarRingsProducts(int cat, List<Product> AllProducts)
-        {
-            //每八個一頁
-            var ProductNumber = 8;
-            //取得目前頁所需要顯示的物品
-            var pList = AllProducts.Skip((cat - 1) * ProductNumber).Take(cat * ProductNumber).ToList();
-
-            //建立全部商品
-            ProductsService service = new ProductsService(db);
-            var AllProductDetails = service.getPageOfProducts();
-            var Model = new List<ProductPageViewModel>();
-
-            for (var i = 0; i < pList.Count; i++)
-            {
-                //避免超出範圍，雖然不一定會用到
-                if (i % 8 == 0 && i != 0) break;
-
-                //取得此產品ID所有的PDID，並加進List<ProductPageViewModel> Model中
-                var NowPid = pList[i].ProductID;
-                var FilterList = AllProductDetails.Where(x => x.ProductID == NowPid);
-                foreach (var item in FilterList)
-                {
-                    Model.Add(item);
-                }
-            }
-            ViewData.Model = Model;
-            return PartialView("_ProductsPartial");
-        }
-
-        [AllowAnonymous]
-        public ActionResult Necklaces(int cat) //項鍊商品頁面
+        public ActionResult Necklaces(int page) //項鍊商品頁面
         {
 
             //抓產品的分類
@@ -128,43 +71,14 @@ namespace ANVI_Mvc.Controllers
 
             //給@Html.Action使用，取得HomeController的GetProducts方法
             ViewBag.Title = "NECKLACES";
-            ViewBag.ActionName = "GetNecklacesProducts";
-            ViewBag.Controller = "Category";
-            ViewBag.Page = cat;
+            ViewBag.ActionName = "GetProducts";
+            ViewBag.Controller = "Home";
+            ViewBag.Page = page;
             return View();
         }
-        [AllowAnonymous]
-        public ActionResult GetNecklacesProducts(int cat, List<Product> AllProducts)
-        {
-            //每八個一頁
-            var ProductNumber = 8;
-            //取得目前頁所需要顯示的物品
-            var pList = AllProducts.Skip((cat - 1) * ProductNumber).Take(cat * ProductNumber).ToList();
-
-            //建立全部商品
-            ProductsService service = new ProductsService(db);
-            var AllProductDetails = service.getPageOfProducts();
-            var Model = new List<ProductPageViewModel>();
-
-            for (var i = 0; i < pList.Count; i++)
-            {
-                //避免超出範圍，雖然不一定會用到
-                if (i % 8 == 0 && i != 0) break;
-
-                //取得此產品ID所有的PDID，並加進List<ProductPageViewModel> Model中
-                var NowPid = pList[i].ProductID;
-                var FilterList = AllProductDetails.Where(x => x.ProductID == NowPid);
-                foreach (var item in FilterList)
-                {
-                    Model.Add(item);
-                }
-            }
-            ViewData.Model = Model;
-            return PartialView("_ProductsPartial");
-        }
 
         [AllowAnonymous]
-        public ActionResult Rings(int cat) //戒指商品頁面
+        public ActionResult Rings(int page) //戒指商品頁面
         {
 
             //抓產品的分類
@@ -176,40 +90,12 @@ namespace ANVI_Mvc.Controllers
 
             //給@Html.Action使用，取得HomeController的GetProducts方法
             ViewBag.Title = "RINGS";
-            ViewBag.ActionName = "GetRingsProducts";
-            ViewBag.Controller = "Category";
-            ViewBag.page = cat;
+            ViewBag.ActionName = "GetProducts";
+            ViewBag.Controller = "Home";
+            ViewBag.page = page;
             return View();
         }
-        [AllowAnonymous]
-        public ActionResult GetRingsProducts(int cat, List<Product> AllProducts)
-        {
-            //每八個一頁
-            var ProductNumber = 8;
-            //取得目前頁所需要顯示的物品
-            var pList = AllProducts.Skip((cat - 1) * ProductNumber).Take(cat * ProductNumber).ToList();
-
-            //建立全部商品
-            ProductsService service = new ProductsService(db);
-            var AllProductDetails = service.getPageOfProducts();
-            var Model = new List<ProductPageViewModel>();
-
-            for (var i = 0; i < pList.Count; i++)
-            {
-                //避免超出範圍，雖然不一定會用到
-                if (i % 8 == 0 && i != 0) break;
-
-                //取得此產品ID所有的PDID，並加進List<ProductPageViewModel> Model中
-                var NowPid = pList[i].ProductID;
-                var FilterList = AllProductDetails.Where(x => x.ProductID == NowPid);
-                foreach (var item in FilterList)
-                {
-                    Model.Add(item);
-                }
-            }
-            ViewData.Model = Model;
-            return PartialView("_ProductsPartial");
-        }
+       
 
         //[AllowAnonymous]
         //public ActionResult EarRingsProductsPage(int page = 1) //耳環商品頁面
