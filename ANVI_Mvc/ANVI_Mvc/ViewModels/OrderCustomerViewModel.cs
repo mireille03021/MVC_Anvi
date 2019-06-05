2b975c6be935e8cacf360c0abf0417a4ef3965c6
mireille03021 <33798498+mireille03021@users.noreply.github.com>
2019-06-06 04:38:46 +0800

    新增Order_Customer防呆驗證

--- ANVI_Mvc/ANVI_Mvc/ViewModels/OrderCustomerViewModel.cs
+++ ANVI_Mvc/ANVI_Mvc/ViewModels/OrderCustomerViewModel.cs
@@ -9,22 +9,31 @@ namespace ANVI_Mvc.ViewModels
 {
     public class OrderCustomerViewModel
     {
+        [Display(Name = "姓名")]
         [Required]
         public string CustomerName { get; set; }
+
+        [Display(Name = "手機號碼")]
         [Required]
         public string Phone { get; set; }
         //public string Country { get; set; }
         public string City { get; set; }
+
+        [Display(Name = "電子信箱")]
+        [EmailAddressAttribute]
         [Required]
         public string Email { get; set; }
+
+        [Display(Name = "地址")]
         [Required]
         public string Address { get; set; }
+
+        [Display(Name = "郵遞區號")]
         public string ZipCode { get; set; }
         // public string BankAccount { get; set; }
         //public string CreditCard { get; set; }
 
         //以下是資料庫沒有的欄位
-        [Required]
         public string Bill_CustomerName { get; set; }
         public string Bill_Address { get; set; }
         public string Bill_City { get; set; }
