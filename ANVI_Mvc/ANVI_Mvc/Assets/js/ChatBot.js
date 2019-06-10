1f655a0455b98507424bde03e2973885ad84b245
mireille03021 <33798498+mireille03021@users.noreply.github.com>
2019-06-11 05:03:46 +0800

    新增lineBot帳號

--- ANVI_Mvc/ANVI_Mvc/Assets/js/ChatBot.js
+++ ANVI_Mvc/ANVI_Mvc/Assets/js/ChatBot.js
@@ -19,8 +19,14 @@ $(function () {
         call_to_action: "Message us", // Call to action
         position: "right", // Position may be 'right' or 'left'
     };
-        var proto = document.location.protocol, host = "whatshelp.io", url = proto + "//static." + host;
-        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
-        s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
-        var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
-})();
+    var proto = document.location.protocol,
+        host = "whatshelp.io",
+        url = proto + "//static." + host;
+    var s = document.createElement('script');
+    s.type = 'text/javascript'; s.async = true;
+    s.src = url + '/widget-send-button/js/init.js';
+    s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+    var x = document.getElementsByTagName('script')[0];
+    x.parentNode.insertBefore(s, x);
+    //$("<p></p>").text("@286yoxxl").prependTo(".LyWrap");
+});
