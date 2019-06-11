c2b40a3fa1c6c6c87cf0e8f0fd9ccb22a9ff1ebf
Chan-Feng Tsao <mireille03021@gmail.com>
2019-06-11 21:15:48 +0800

    新增ViewWordModel

---
+++ ANVI_Mvc/ANVI_Mvc/Models/ViewWord.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ANVI_Mvc.Models
+{
+    public class ViewWord
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(128)]
+        public string WordName { get; set; }
+
+        [Required]
+        public string WordContent { get; set; }
+    }
+}
