3692cec14759b6fe43cdc35c3c0514536944dc58
mireille03021 <33798498+mireille03021@users.noreply.github.com>
2019-06-13 01:16:48 +0800

    修正lineBot

--- ANVI_Mvc/ANVI_Mvc/Assets/js/ChatBot.js
+++ ANVI_Mvc/ANVI_Mvc/Assets/js/ChatBot.js
@@ -1,21 +1,6 @@
-﻿//$(function () {
-//    var options = {
-//        facebook: "232472107265920", // Facebook page ID
-//        line: "//line.me/ti/p/2Y-7aApB8d", // Line QR code URL
-//        call_to_action: "點這跟我們聊聊！", // Call to action
-//        button_color: "#F0E68C", // Color of button
-//        position: "right", // Position may be 'right' or 'left'
-//        order: "facebook,line", // Order of buttons
-//    };
-//    var proto = document.location.protocol, host = "whatshelp.io", url = proto + "//static." + host;
-//    var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
-//    s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
-//    var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
-//})();
-
-$(function () {
+﻿$(function () {
     var options = {
-        line: "//line.me/R/ti/p/%40286yoxxl", // Line QR code URL
+        line: "//line.me/R/ti/p/%40545nfrgg", // Line QR code URL
         call_to_action: "Line QRCode在這！", // Call to action
         position: "right", // Position may be 'right' or 'left'
     };
@@ -23,10 +8,10 @@ $(function () {
         host = "whatshelp.io",
         url = proto + "//static." + host;
     var s = document.createElement('script');
-    s.type = 'text/javascript'; s.async = true;
+    s.type = 'text/javascript';
+    s.async = true;
     s.src = url + '/widget-send-button/js/init.js';
     s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
     var x = document.getElementsByTagName('script')[0];
     x.parentNode.insertBefore(s, x);
-    //$("<p></p>").text("@286yoxxl").prependTo(".LyWrap");
 });
